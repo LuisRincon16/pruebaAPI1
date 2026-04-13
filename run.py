e7319ca0412f073a6680fbdc914553a623b61f45
LuisRincon16 <lurinconj@unal.edu.co>
2026-04-12 22:32:00 -0500

    corrigiendo error de arranque

--- run.py
+++ run.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from app.BD.BDapi import BaseDeDatos
 
+app = Flask(__name__)
+CORS(app)  # Permite peticiones desde Android
+db = BaseDeDatos()
+
+# Registrar los "blueprints" (grupos de rutas)
+from app.routes.usuarios import usuarios_bp
+app.register_blueprint(usuarios_bp, url_prefix="/api/usuarios")
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    CORS(app)  # Permite peticiones desde Android
-    db = BaseDeDatos()
-
-    # Registrar los "blueprints" (grupos de rutas)
-    from app.routes.usuarios import usuarios_bp
-    app.register_blueprint(usuarios_bp, url_prefix="/api/usuarios")
-    
     app.run()
