f56a83eacc083664cb68f59755bbfa721d975106
LuisRincon16 <lurinconj@unal.edu.co>
2026-04-18 00:54:12 -0500

    ATRAPANDO EXCEPCIONES DE TABLAS NO CREADAS

--- app/BD/BDapi.py
+++ app/BD/BDapi.py
@@ -25,7 +25,6 @@ class BaseDeDatos():
             self.conexion = sqlitecloud.connect(self.url)
             return True
         except Exception as e:
-            #print(f"Error al conectar a la base de datos: {e}")
             self.conexion = None
             return False
 
@@ -35,121 +34,129 @@ class BaseDeDatos():
                 self.conexion.close()
                 self.conexion = None
             except Exception as e:
-                #print(f"Error al desconectar de la base de datos: {e}")
                 self.conexion = None
-                pass
 
     def crear_tablas(self, nombre_tabla=["compras", "gastos", "prestamos"]):
-        try:
-            self.conectar()
-            cursor = self.conexion.cursor()
-            for table in nombre_tabla:
-                cursor.execute(f"""
-                    CREATE TABLE IF NOT EXISTS {table} (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        descripcion VARCHAR NOT NULL,
-                        valor INTEGER NOT NULL,
-                        fecha VARCHAR NOT NULL,
-                        hora VARCHAR NOT NULL,
-                        tipo VARCHAR NOT NULL )""")
-            self.conexion.commit()
-            self.desconectar()
-        except Exception as e:
-            #print(f"Error al crear las tablas: {e}")
-            self.desconectar()
+        result_cone = self.conectar()
+        if result_cone:
+            try:
+                cursor = self.conexion.cursor()
+                for table in nombre_tabla:
+                    cursor.execute(f"""
+                        CREATE TABLE IF NOT EXISTS {table} (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            descripcion VARCHAR NOT NULL,
+                            valor INTEGER NOT NULL,
+                            fecha VARCHAR NOT NULL,
+                            hora VARCHAR NOT NULL,
+                            tipo VARCHAR NOT NULL )""")
+                self.conexion.commit()
+                self.desconectar()
+            except Exception as e:
+                self.desconectar()
 
     def agregar_dato(self, nombre_tabla, descripcion, valor):
         ahora = datetime.now(self.zonaHorariaColombia)
         fecha = ahora.strftime("%Y-%m-%d")
         hora = ahora.strftime("%H:%M:%S")
 
-        #print(f"\nAgregando a {nombre_tabla}: {descripcion} - {valor} el {fecha} a las {hora}\n")
         tipo_ajustado = None
         if nombre_tabla == "COMPRAS":
-            tipo_ajustado = "Com."
+            tipo_ajustado = "C"
         elif nombre_tabla == "GASTOS":
-            tipo_ajustado = "Gas."
+            tipo_ajustado = "G"
         elif nombre_tabla == "PRESTAMOS":
-            tipo_ajustado = "Pres."
+            tipo_ajustado = "P"
 
-        conexion = self.conectar()
-        if conexion:
-            cursor = self.conexion.cursor()
-            cursor.execute(f"INSERT INTO {nombre_tabla} (descripcion, valor, fecha, hora, tipo) VALUES (?, ?, ?, ?, ?)", (descripcion, valor, fecha, hora, tipo_ajustado))
-            self.conexion.commit()
-            self.desconectar()
-            return True
+        cone = self.conectar()
+        if cone:
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute(f"INSERT INTO {nombre_tabla} (descripcion, valor, fecha, hora, tipo) VALUES (?, ?, ?, ?, ?)", (descripcion, valor, fecha, hora, tipo_ajustado))
+                self.conexion.commit()
+                self.desconectar()
+                return True
+            except Exception as e:
+                self.desconectar()
+                return False
         else:
             return False
 
     # --------- Vistas e índices para optimizar consultas del historial ---------
 
     def crear_vista_historial(self):
-        try:
-            self.conectar()
-            cursor = self.conexion.cursor()
-            cursor.execute("""
-                CREATE VIEW IF NOT EXISTS historial_completo AS
-                SELECT * FROM compras
-                UNION ALL
-                SELECT * FROM gastos
-                UNION ALL
-                SELECT * FROM prestamos
-            """)
-            self.conexion.commit()
-            self.desconectar()
-        except Exception as e:
-            #print(f"Error al crear la vista del historial: {e}")
-            self.desconectar()
+        cone = self.conectar()
+        if cone:
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("""
+                    CREATE VIEW IF NOT EXISTS historial_completo AS
+                    SELECT * FROM compras
+                    UNION ALL
+                    SELECT * FROM gastos
+                    UNION ALL
+                    SELECT * FROM prestamos
+                """)
+                self.conexion.commit()
+                self.desconectar()
+            except Exception as e:
+                self.desconectar()
 
     def crear_indice_fecha_hora(self):
-        try:
-            self.conectar()
-            cursor = self.conexion.cursor()
-            for table in ["compras", "gastos", "prestamos"]:
-                cursor.execute(f"""CREATE INDEX IF NOT EXISTS
-                                    idx_{table}_fecha_hora ON {table}(fecha, hora)""")
-            self.conexion.commit()
-            self.desconectar()
-        except Exception as e:
-            #print(f"Error al crear los índices de fecha y hora: {e}")
-            self.desconectar()
+        cone = self.conectar()
+        if cone:
+            try:
+                cursor = self.conexion.cursor()
+                for table in ["compras", "gastos", "prestamos"]:
+                    cursor.execute(f"""CREATE INDEX IF NOT EXISTS
+                                        idx_{table}_fecha_hora ON {table}(fecha, hora)""")
+                self.conexion.commit()
+                self.desconectar()
+            except Exception as e:
+                self.desconectar()
 
     # --------- método para obtener datos del historial ---------
 
     def obtener_historial(self, fecha_inicio, fecha_fin, descripcion):
         result_conexion = self.conectar()
         if result_conexion:
-            self.conexion.row_factory = sqlitecloud.Row
-            cursor = self.conexion.cursor()
-            if descripcion is None:
-                cursor.execute("""SELECT * 
-                                    FROM historial_completo
-                                    WHERE fecha BETWEEN ? AND ?
-                                    ORDER BY fecha DESC, hora DESC""", (fecha_inicio, fecha_fin))
-            else:
-                cursor.execute("""SELECT * 
-                                    FROM historial_completo
-                                    WHERE fecha BETWEEN ? AND ?
-                                    AND descripcion LIKE ?
-                                    ORDER BY fecha DESC, hora DESC""", (fecha_inicio, fecha_fin, f"%{descripcion}%"))
-                
-            datos = cursor.fetchall()
-            self.desconectar()
-            return [dict(row) for row in datos]
+            try:
+                self.conexion.row_factory = sqlitecloud.Row
+                cursor = self.conexion.cursor()
+                if descripcion is None:
+                    cursor.execute("""SELECT * 
+                                        FROM historial_completo
+                                        WHERE fecha BETWEEN ? AND ?
+                                        ORDER BY fecha DESC, hora DESC""", (fecha_inicio, fecha_fin))
+                else:
+                    cursor.execute("""SELECT * 
+                                        FROM historial_completo
+                                        WHERE fecha BETWEEN ? AND ?
+                                        AND descripcion LIKE ?
+                                        ORDER BY fecha DESC, hora DESC""", (fecha_inicio, fecha_fin, f"%{descripcion}%"))
+                    
+                datos = cursor.fetchall()
+                self.desconectar()
+                return [dict(row) for row in datos]
+            except Exception as e:
+                self.desconectar()
+                return None
         else:
             return None
         
-
     def consultar_opciones(self, nombre_tabla):
-        result_conexion = self.conectar()
-        if result_conexion:
-            cursor = self.conexion.cursor()
-            cursor.execute(f"""SELECT DISTINCT descripcion 
-                                FROM {nombre_tabla}""")
-            datos = cursor.fetchall()
-            self.desconectar()
-            return [row[0] for row in datos]
+        cone = self.conectar()
+        if cone:
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute(f"""SELECT DISTINCT descripcion 
+                                    FROM {nombre_tabla}""")
+                datos = cursor.fetchall()
+                self.desconectar()
+                return [row[0] for row in datos]
+            except Exception as e:
+                self.desconectar()
+                return None
         else:
             return None
 
